60699609e757852d27869b7488c199e4159fc81b
Marcus More-Thiesen <marcus.morethiesen@bcgdv.com>
2019-10-15 13:47:34 +0200

    Generated by start.spring.io

---
+++ src/main/kotlin/com/example/grapqhl/GrapqhlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.grapqhl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrapqhlApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrapqhlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/grapqhl/GrapqhlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.grapqhl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrapqhlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
